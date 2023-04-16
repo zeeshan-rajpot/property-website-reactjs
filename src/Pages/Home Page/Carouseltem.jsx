242b84791c72d5a279082daa205711d6056b37b4
Zeeshan <zeeshanrajpot74@gmail.com>
2023-04-16 22:03:53 +0500

    second commit

--- src/Pages/Home Page/Carouseltem.jsx
+++ src/Pages/Home Page/Carouseltem.jsx
@@ -1,32 +1,44 @@
-import React from 'react'
-import Carousel from 'react-grid-carousel';
-import './CarouselItm.css'
+import React from "react";
+import Carousel from "react-grid-carousel";
+import "./CarouselItm.css";
 import { Container, Col, Row } from "react-bootstrap";
-import Card from 'react-bootstrap/Card';
+import Card from "react-bootstrap/Card";
 const Cards = (props) => {
   return (
     <div>
       <Container>
-        <Row >
-      
-        <Col >
-
-          <Card>
-            <span className='blurbackground' style={{position:'absolute', marginTop:'51%' ,marginBottom:'10px'}}>{props.properties}</span>
-           <span className=' displayflexmobile blur' style={{position:'absolute', marginTop:'47%', marginLeft:'7%'  , color:'white'}}>
-            <span  > {props.location}</span>
-            <span  > 78</span>
-            </span>
-               <Card.Img variant="top" src={props.path} className='boderradius' />
-       
-              
-      </Card>
-        </Col>
-     
-    </Row>
-    </Container>
+        <Row>
+          <Col>
+            <Card>
+              <span
+                className="blurbackground"
+                style={{ position: "absolute", marginTop: "87%" }}
+              >
+                {props.properties}
+              </span>
+              <span
+                className=" displayflexmobile blur"
+                style={{
+                  position: "absolute",
+                  marginTop: "79%",
+                  marginLeft: "7%",
+                  color: "white",
+                }}
+              >
+                <span className="location"> {props.location}</span>
+                <span className="number"> 78</span>
+              </span>
+              <Card.Img
+                variant="top"
+                src={props.path}
+                className="boderradius"
+              />
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Cards
+export default Cards;
